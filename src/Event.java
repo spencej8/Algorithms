645493a846b66e9bb408618ee9b05c832230da9a
spencej8 <spencej8@miamioh.edu>
2018-04-24 16:36:34 -0400

    Added additional attributes

--- src/Event.java
+++ src/Event.java
@@ -2,5 +2,16 @@ import java.util.Date;
 
 public class Event 
 {
-	private Date time;
+	public Date timeOfEvent;
+	
+	public String location;
+	
+	public int numOfBartendersNeeded;
+	
+	public int numOfServersNeeded;
+	
+	public int numOfServerAssistantsNeeded;
+	
+	public int numOfCooksNeeded;
+	
 }
---
+++ src/StaffMember.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+import java.util.LinkedList;
+
+public class StaffMember 
+{
+	public LinkedList<String> position;
+	
+	public LinkedList<String> locationsAbleToWork;
+	
+	public LinkedList<Date> availableTimeSlots;
+	
+	public boolean isRequested;
+	
+	public int consecutiveSkips;
+	
+	public boolean isSuspended;
+	
+	public int rating;
+	
+	public int experience;
+	
+	public double weight;
+}
